35b255e86b1c0a4ce74b57d7d7c5d37ff31abc7b
Vaishnavi <firststarprincess@gmail.com>
2023-10-01 21:28:33 +0530

    done read temperature and alert functions

--- src/agents/temperaturealert/tempagent.py
+++ src/agents/temperaturealert/tempagent.py
@@ -1,62 +1,53 @@
-from uagents import Agent
+from uagents import Agent,Context,Protocol
 import requests
 import os
 import geocoder
 from dotenv import load_dotenv
+from uagents.setup import fund_agent_if_low
 
 load_dotenv()
 
 # Get API_KEY from .env
 api_key = os.getenv("API_KEY")
-class TemperatureAlert(Agent):
-    def __init__(self, location, min_temp, max_temp):
-        super().__init__()
-        self.location = location
-        self.min_temp = min_temp
-        self.max_temp = max_temp
 
-        self.curtemp = None
+def read_temp():
+    loca = "Chennai, IN"
+    response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?APPID={api_key}&q={loca}&units=metric')
+    curtemp = None
+    if response.status_code != 200:
+        raise Exception('Could not get current temperature')
+    else:
+        curtemp = (response.json()['main']['temp']) # Retrieve current Celcius temperature
+    return curtemp
+
+def alert_check(curtemp,min_temp,max_temp):
+    if curtemp is None:
+        read_temp()
     
-    def read_temp(self):
-        loca = self.location.get_location()
-        response = requests.get(f'https://api.openweathermap.org/data/2.5/weather?APPID={api_key}&q={loca}')
-        if response.status_code != 200:
-            raise Exception('Could not get current temperature')
-        else:
-            self.curtemp = response.json()['main']['temp'] # Retrieve current Celcius temperature
-            print("Current temperature:",self.curtemp)
-    
-    def alert_check(self):
-        if self.curtemp is None:
-            self.read_temp()
-        
-        elif self.curtemp < self.min_temp or self.curtemp > self.max_temp:
-            print('Temperature in your location is outside your preferred range!!')
-    
-    def change_range(self,min_temp,max_temp):
-        if min_temp > max_temp:
-            raise Exception("Error! Minimum temperature cannot be greater than maximum temperature!!")
-        self.min_temp = min_temp
-        self.max_temp = max_temp
+    elif curtemp < min_temp or curtemp > max_temp:
+        print('Temperature in your location is outside your preferred range!!')
 
-class Location(Agent):
-    def __init__(self):
-        super().__init__()
-        self.location = None
-    
-    def get_location(self):
-        curloc = geocoder.ip('me')
-        self.location = curloc.latlng
-        return self.location
+# def change_range(mint,maxt):
+#     if mint > maxt:
+#         raise Exception("Error! Minimum temperature cannot be greater than maximum temperature!!")
+#     min_temp = mint
+#     max_temp = maxt
 
+temp = Agent(
+    name='tempalertagent',
+    seed='nil temperature',
+    port=8000,
+    endpoint=["http://127.0.0.1:8000"]
+    )
+fund_agent_if_low(temp.wallet.address())
 
+# Print agent address
+print("uAgent address: ", temp.address)
 
-# temp = Agent(
-#     name='detecttemp',
-#     seed='nil temperature',
-#     )
+@temp.on_interval(period=1.0) # Check temperature every microsecond
+async def check(ctx:Context):
+    cur_temperature = read_temp()
+    ctx.logger.info(f"Current temperature: {cur_temperature}")
+    alert_check(cur_temperature,20,30)
 
-# location = Agent(
-#     name='location',
-#     seed='none'
-# )
+temp.run()
